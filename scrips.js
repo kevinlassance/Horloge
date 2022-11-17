5ef845cd2308f178aae74357d0793553b8032d77
kevinlassance <lassancekevin100@gmail.com>
2022-11-17 13:39:02 +0100

    Finish

--- scrips.js
+++ scrips.js
@@ -1,4 +1,21 @@
-const handleHour = document.getElementsByClassName("hour-hand");
-const handleMinutes = document.getElementsByClassName("min-hand");
-const handleSeconds = document.getElementsByClassName("second-hand");
+const secondsHtml = document.querySelector(".second-hand");
+const minutesHtml = document.querySelector(".min-hand");
+const hoursHtml = document.querySelector(".hour-hand");
 
+function setDate() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondsDegrees = (seconds / 60) * 360 + 90;
+	secondsHtml.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const minutes = now.getMinutes();
+	const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+	minutesHtml.style.transform = `rotate(${minutesDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+	hoursHtml.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+setDate();
